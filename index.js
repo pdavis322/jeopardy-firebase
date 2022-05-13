fd57a98f2dc1086186f5506f75f25ac0ea7308c0
pdavis322 <26099766+pdavis322@users.noreply.github.com>
2022-05-13 15:01:35 -0400

    added firebase files

---
+++ index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBapcnEfaY_NDYAHxhQC2y3hQ7fsvAXv3A",
+  authDomain: "jeopardy-a1ef4.firebaseapp.com",
+  projectId: "jeopardy-a1ef4",
+  storageBucket: "jeopardy-a1ef4.appspot.com",
+  messagingSenderId: "463885909483",
+  appId: "1:463885909483:web:740022911adfa7835d2dae"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
